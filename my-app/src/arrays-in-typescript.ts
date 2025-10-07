c0232ff30f0c7027b5b3f6139d8660d62f8f39df
mareola-mabs <bulun1001@gmail.com>
2025-10-08 00:10:21 +0100

    chore: add some more notes on arrays

--- my-app/src/arrays-in-typescript.ts
+++ my-app/src/arrays-in-typescript.ts
@@ -2,7 +2,13 @@
 
 const books:(string | number)[] = [1984, 'Brave New World', 'Fahrenheit 451'] // String or Number type
 
-let numbers: Array<number | string> = [1, 2, 3] // String or Number type
+let stringOrNumbers: Array<number | string> = [1, 2, 3] // String or Number type
+
+let numbers: number[] = [4, 5, 6] // Accepts only numbers
+
+// numbers = [4, 5, 6, 'Femi' Won't work Accepts only numbers
+
+console.log(numbers, stringOrNumbers)
 
 // let other:[] = ['item1'] This throws an error, expects empty values
 
