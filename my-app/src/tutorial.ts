f40dca8716a3cc1a48a723c4c6c73579dc1cecbf
Mareola-Mabs <bulun1001@gmail.com>
2025-10-12 01:20:20 +0100

    feat: add example file

---
+++ my-app/src/tutorial.ts
@@ -0,0 +1,14 @@
+console.log("typescript tutorial")
+interface someValue{
+    name:string
+    id:number
+}
+
+let someObj: someValue = {
+    name: "Mareola",
+    id: 1
+}
+
+console.log(someObj);
+
+alert('dad')
